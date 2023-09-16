cd4cbe493c26f893e8ccb804620e4ef5052a24ef
Danial <76565771+DanialHaseeb@users.noreply.github.com>
2023-09-16 10:39:08 +0500

    Update main.c

--- main.c
+++ main.c
@@ -12,82 +12,78 @@ bool newGrid[rows][cols] = {false};
 
 void setup()
 {
-    for (int i = 0; i < rows; ++i)
-    {
-        for (int j = 0; j < cols; ++j)
-        { grid[i][j] = (GetRandomValue(0, 1) == 1); }
-    }
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j < cols; ++j)
+		{ grid[i][j] = (GetRandomValue(0, 1) == 1); }
+	}
 }
 
 int countNeighbors(int x, int y)
 {
-    int count = 0;
-    for (int i = -1; i <= 1; ++i)
-    {
-        for (int j = -1; j <= 1; ++j)
-        {
-            int row = (x + i + rows) % rows;
-            int col = (y + j + cols) % cols;
-            count += grid[row][col];
-        }
-    }
-    return count - grid[x][y];
+	int count = 0;
+	for (int i = -1; i <= 1; ++i)
+	{
+		for (int j = -1; j <= 1; ++j)
+		{
+			int row = (x + i + rows) % rows;
+			int col = (y + j + cols) % cols;
+			count += grid[row][col];
+		}
+	}
+	return count - grid[x][y];
 }
 
 void update()
 {
-    for (int i = 0; i < rows; ++i)
-    {
-        for (int j = 0; j < cols; ++j)
-        {
-            int neighbors = countNeighbors(i, j);
-          
-            newGrid[i][j] = grid[i][j];
-          
-            if ((grid[i][j]) && (neighbors < 2 || neighbors > 3))
-            { newGrid[i][j] = false; }
-            else if (neighbors == 3)
-            { newGrid[i][j] = true; }
-        }
-    }
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j < cols; ++j)
+		{
+			int neighbors = countNeighbors(i, j);
+			newGrid[i][j] = grid[i][j];
+			if (grid[i][j] && (neighbors < 2 || neighbors > 3)) { newGrid[i][j] = false; }
+			else if (!grid[i][j] && neighbors == 3) { newGrid[i][j] = true; }
+		}
+	}
 
-    for (int i = 0; i < rows; ++i)
-    {
-        for (int j = 0; j < cols; ++j)
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j < cols; ++j)
         { grid[i][j] = newGrid[i][j]; }
-    }
+	}
 }
 
 void draw()
 {
-    for (int i = 0; i < rows; ++i)
-    {
-        for (int j = 0; j < cols; ++j)
-        {
-            if (grid[i][j])
+	for (int i = 0; i < rows; ++i)
+	{
+		for (int j = 0; j < cols; ++j)
+		{
+			if (grid[i][j])
             { DrawRectangle(j * cellSize, i * cellSize, cellSize, cellSize, WHITE); }
-        }
-    }
+		}
+	}
 }
 
 int main()
 {
-    InitWindow(width, height, "Game of Life");
-    setup();
+	InitWindow(width, height, "Game of Life");
+	setup();
 
-    SetTargetFPS(10);
+	SetTargetFPS(10);
 
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(BLACK);
+	while (!WindowShouldClose())
+	{
+	    BeginDrawing();
+		ClearBackground(BLACK);
 
-        draw();
-        update();
+		draw();
+		update();
 
-        EndDrawing();
-    }
+		EndDrawing();
+	}
 
-    CloseWindow();
-    return 0;
+	CloseWindow();
+	return 0;
 }
